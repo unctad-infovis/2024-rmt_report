9f61f3d121ba9e32ca9aa624a973e180eccd91e3
Teemo Tebest <teemo.tebest@gmail.com>
2024-10-25 16:41:21 +0200

    Updated

--- src/jsx/Footer.jsx
+++ src/jsx/Footer.jsx
@@ -15,65 +15,75 @@ function Footer() {
         <div className="footer_elements">
           <div className="footer_element footer_element_1">
             <div className="footer_content anchor_videos" id="anchor_videos">
-              {/*<
-              h3>Watch the videos</h3>
+              <h3>Watch the videos</h3>
               <div className="iframe_container youtube_iframe">
-                <iframe src="https://www.youtube.com/embed/0iVoEdQ8rAc" title="Review of Maritime Transport 2024" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen />
+                <iframe src="https://www.youtube.com/embed/PLzwchtLJSk" title="Review of Maritime Transport 2024" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen />
               </div>
-              */}
-              {/*
               <ul>
                 <li>
-                  <a href="#download" target="_blank" rel="noreferrer">Français</a>
+                  <a href="https://youtu.be/gH6i1venniM" target="_blank" rel="noreferrer">Français</a>
                   {', '}
-                  <a href="#download" target="_blank" rel="noreferrer">Español</a>
+                  <a href="https://youtu.be/sJ-wxXwmSlg" target="_blank" rel="noreferrer">Español</a>
                   {', '}
-                  <a href="#download" target="_blank" rel="noreferrer">العربية</a>
+                  <a href="https://youtu.be/FTeIOjzp-n0" target="_blank" rel="noreferrer">العربية</a>
                   {', '}
-                  <a href="#download" target="_blank" rel="noreferrer">简体中文</a>
+                  <a href="https://youtu.be/1g-ia7rZUEs" target="_blank" rel="noreferrer">简体中文</a>
                   {', '}
-                  <a href="#download" target="_blank" rel="noreferrer">Português</a>
+                  <a href="https://youtu.be/wBrYD3gfPRc" target="_blank" rel="noreferrer">Русский</a>
                   {', '}
-                  <a href="#download" target="_blank" rel="noreferrer">Kiswahili</a>
+                  <a href="https://youtu.be/oLIUDxnL3hg" target="_blank" rel="noreferrer">Português</a>
                   {', '}
-                  <a href="#download" target="_blank" rel="noreferrer">Hindi हिंदी</a>
+                  <a href="https://youtu.be/qzywOEqyvwI" target="_blank" rel="noreferrer">Kiswahili</a>
+                  {', '}
+                  <a href="https://youtu.be/hmkbO7Z2kVs" target="_blank" rel="noreferrer">Urdu اردو</a>
+                  {', '}
+                  <a href="https://youtu.be/IxnQGmlbXGw" target="_blank" rel="noreferrer">Hindi हिंदी</a>
+                  {', '}
+                  <a href="https://youtu.be/siCd0BVBh_c" target="_blank" rel="noreferrer">Deutsch</a>
                 </li>
               </ul>
-              */}
-              {/*
               <h4>Download the report video</h4>
               <ul>
                 <li>
-                  <a href="#download" target="_blank" rel="noreferrer">English</a>
+                  <a href="https://vimeo.com/1022047383" target="_blank" rel="noreferrer">English</a>
                   {', '}
-                  <a href="#download" target="_blank" rel="noreferrer">Français</a>
+                  <a href="https://trello.com/1/cards/66ea989dbf21c10916cdffb1/attachments/6717cbfc23920bed26e18643/download/RMT_French.mp4" target="_blank" rel="noreferrer">Français</a>
                   {', '}
-                  <a href="#download" target="_blank" rel="noreferrer">Español</a>
+                  <a href="https://trello.com/1/cards/66ea989dbf21c10916cdffb1/attachments/6717cbe5ba1d4adf22b43d40/download/RMT_Spanish.mp4" target="_blank" rel="noreferrer">Español</a>
                   {', '}
-                  <a href="#download" target="_blank" rel="noreferrer">العربية</a>
+                  <a href="https://trello.com/1/cards/66ea989dbf21c10916cdffb1/attachments/6717cc0713e9a0526177d065/download/RMT_Arabic.mp4" target="_blank" rel="noreferrer">العربية</a>
                   {', '}
-                  <a href="#download" target="_blank" rel="noreferrer">简体中文</a>
+                  <a href="https://trello.com/1/cards/66ea989dbf21c10916cdffb1/attachments/6717cbf14fef798388c7b1e8/download/RMT_Chinese.mp4" target="_blank" rel="noreferrer">简体中文</a>
                   {', '}
-                  <a href="#download" target="_blank" rel="noreferrer">Português</a>
+                  <a href="https://trello.com/1/cards/66ea989dbf21c10916cdffb1/attachments/6717f4408702f714aec05406/download/RMT_Russian.mp4" target="_blank" rel="noreferrer">Русский</a>
                   {', '}
-                  <a href="#download" target="_blank" rel="noreferrer">Kiswahili</a>
+                  <a href="https://trello.com/1/cards/66ea989dbf21c10916cdffb1/attachments/6717f4627791536fa678c968/download/RMT_Portuguese.mp44" target="_blank" rel="noreferrer">Português</a>
                   {', '}
-                  <a href="#download" target="_blank" rel="noreferrer">Hindi हिंदी</a>
+                  <a href="https://trello.com/1/cards/66ea989dbf21c10916cdffb1/attachments/6717f46dd04095240ee8e7ee/download/RMT_Kiswahili.mp4" target="_blank" rel="noreferrer">Kiswahili</a>
+                  {', '}
+                  <a href="https://trello.com/1/cards/66ea989dbf21c10916cdffb1/attachments/6717f478e550b873467fcd57/download/RMT_Urdu.mp4" target="_blank" rel="noreferrer">Urdu اردو</a>
+                  {', '}
+                  <a href="https://trello.com/1/cards/66ea989dbf21c10916cdffb1/attachments/6717f4577bf03fefc8ffb06b/download/RMT_Hindi.mp4" target="_blank" rel="noreferrer">Hindi हिंदी</a>
+                  {', '}
+                  <a href="https://trello.com/1/cards/66ea989dbf21c10916cdffb1/attachments/6717f44b00de797a3f7d0fd3/download/RMT_German.mp4" target="_blank" rel="noreferrer">Deutsch</a>
                 </li>
               </ul>
-              */}
-              {/*
               <h4>Watch the press conference</h4>
               <div className="iframe_container youtube_iframe">
-                <iframe src="https://www.youtube.com/embed/w0H1eCzE0LY" title="Digital economy report 2024 press conference" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen />
+                <iframe src="https://www.youtube.com/embed/AYD39tO7VGM" title="Review of Maritime Transport 2024 press conference" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen />
               </div>
-              <h4>Download the video</h4>
+              <h4>Media assets</h4>
               <ul>
                 <li>
-                  <a href="#download " target="_blank" rel="noreferrer">English</a>
+                  <a href="https://vimeo.com/1022207903" target="_blank" rel="noreferrer">Press conference</a>
+                </li>
+                <li>
+                  <a href="https://www.flickr.com/photos/unctad/albums/72177720321427679" target="_blank" rel="noreferrer">Photos</a>
+                </li>
+                <li>
+                  <a href="https://www.unognewsroom.org/story/en/2395/unctad-press-conference-22-october-2024" target="_blank" rel="noreferrer">Audio and other assets</a>
                 </li>
               </ul>
-              */}
             </div>
             <div className="footer_content anchor_podcasts" id="anchor_podcasts">
               <h3>Podcasts</h3>
