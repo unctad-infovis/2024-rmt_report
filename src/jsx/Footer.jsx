ac915da637fb2730560c1dd0f30edd0537e54ae6
Teemo Tebest <teemo.tebest@gmail.com>
2026-01-30 12:11:52 +0100

    Updated

--- src/jsx/Footer.jsx
+++ src/jsx/Footer.jsx
@@ -17,7 +17,7 @@ function Footer() {
             <div className="footer_content anchor_videos" id="anchor_videos">
               <h3>Watch the videos</h3>
               <div className="iframe_container youtube_iframe">
-                <iframe src="https://www.youtube.com/embed/PLzwchtLJSk" title="Review of Maritime Transport 2024" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen />
+                <iframe src="https://www.youtube.com/embed/PLzwchtLJSk" title="Review of Maritime Transport 2024" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen referrerPolicy="strict-origin-when-cross-origin" />
               </div>
               <ul>
                 <li>
@@ -70,7 +70,7 @@ function Footer() {
               </ul>
               <h4>Watch the press conference</h4>
               <div className="iframe_container youtube_iframe">
-                <iframe src="https://www.youtube.com/embed/AYD39tO7VGM" title="Review of Maritime Transport 2024 press conference" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen />
+                <iframe src="https://www.youtube.com/embed/AYD39tO7VGM" title="Review of Maritime Transport 2024 press conference" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen referrerPolicy="strict-origin-when-cross-origin" />
               </div>
               <h4>Media assets</h4>
               <ul>
@@ -89,7 +89,7 @@ function Footer() {
               <h3>Podcasts</h3>
               <p>Listen to the Weekly Tradecast episodes that explore some of the main issues in the report</p>
               <div className="iframe_container">
-                <iframe title="96. Rough seas ahead: How can global supply chains navigate shipping disruptions?" height="150" width="100%" style={{ border: 'none', minWidth: 'min(100%, 430px)' }} scrolling="no" data-name="pb-iframe-player" src="https://www.podbean.com/player-v2/?i=zix3p-161db2f-pb&btn-skin=009EDB&download=1&font-color=000000&fonts=Verdana&from=pb6admin&logo_link=none&rtl=0&share=1&size=240&skin=ffffff" allowFullScreen />
+                <iframe title="96. Rough seas ahead: How can global supply chains navigate shipping disruptions?" height="150" width="100%" style={{ border: 'none', minWidth: 'min(100%, 430px)' }} scrolling="no" data-name="pb-iframe-player" src="https://www.podbean.com/player-v2/?i=zix3p-161db2f-pb&btn-skin=009EDB&download=1&font-color=000000&fonts=Verdana&from=pb6admin&logo_link=none&rtl=0&share=1&size=240&skin=ffffff" allowFullScreen referrerPolicy="strict-origin-when-cross-origin" />
                 <span className="text"><a href="/podcast/rough-seas-ahead-how-can-global-supply-chains-navigate-shipping-disruptions">Rough seas ahead: How can global supply chains navigate shipping disruptions</a></span>
               </div>
             </div>
